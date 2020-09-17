e8b9d97543b709f4cae3664680ae42c1c7bc4689
Yoshinori Kawasaki <yoshi@wantedly.com>
2020-09-17 18:28:41 +0900

    bundle gem activerecord-lookml

---
+++ lib/activerecord/lookml.rb
@@ -0,0 +1,8 @@
+require "activerecord/lookml/version"
+
+module Activerecord
+  module Lookml
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/activerecord/lookml_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Activerecord::Lookml do
+  it "has a version number" do
+    expect(Activerecord::Lookml::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
