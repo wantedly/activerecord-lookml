a66f236652b0fc69d1f31af0bcfe1d04b43468e6
Yoshinori Kawasaki <yoshi@wantedly.com>
2021-05-27 17:50:49 +0900

    Create dummy schema.rb

---
+++ spec/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema.define(version: 2021_05_26_103300) do
+  create_table "pulse_onboarding_statuses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.integer "manager_tutorial_state"
+    t.integer "member_tutorial_state"
+    t.boolean "completed", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+end
