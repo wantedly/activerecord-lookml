94cae384a77e0d6910161a2c913d5d9ed4d3d714
Yoshinori Kawasaki <yoshi@wantedly.com>
2021-06-03 17:32:02 +0900

    Update spec for string column

--- spec/schema.rb
+++ spec/schema.rb
@@ -4,6 +4,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_103300) do
     t.bigint "company_id", null: false
     t.integer "manager_tutorial_state"
     t.integer "member_tutorial_state"
+    t.string "session_id"
     t.boolean "completed", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
