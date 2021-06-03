ab50b64ba4fd09becfb970d75825cbb4f871065e
Yoshinori Kawasaki <yoshi@wantedly.com>
2021-06-03 16:52:40 +0900

    Remove #enum_to_lookml method

--- lib/active_record/lookml/core.rb
+++ lib/active_record/lookml/core.rb
@@ -4,81 +4,6 @@ module ActiveRecord
       extend ActiveSupport::Concern
 
       module ClassMethods
-        # Generates LookML dimension from ruby hash.
-        #
-        # Input format:
-        #
-        # {"manager_tutorial_state"=>
-        #   {"start_tutorial"=>0,
-        #    "explain_condition_survay"=>1,
-        #    "ask_condition"=>2,
-        #    "explain_high_fives"=>3,
-        #    "try_high_fives"=>4,
-        #    "request_members_to_get_started"=>5,
-        #    "done_announce_to_member"=>6,
-        #    "done_tutorial"=>7}}
-        #
-        #
-        # Output format:
-        #
-        # dimension: manager_tutorial_state {
-        #   case: {
-        #       when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 0 ;;
-        #       label: "start_tutorial"
-        #     }
-        #     when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 1 ;;
-        #       label: "explain_condition_survay"
-        #     }
-        #     when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 2 ;;
-        #       label: "ask_condition"
-        #     }
-        #     when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 3 ;;
-        #       label: "explain_high_fives"
-        #     }
-        #     when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 4 ;;
-        #       label: "try_high_fives"
-        #     }
-        #     when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 5 ;;
-        #       label: "request_members_to_get_started"
-        #     }
-        #     when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 6 ;;
-        #       label: "done_announce_to_member"
-        #     }
-        #     when: {
-        #       sql: ${TABLE}.manager_tutorial_state = 7 ;;
-        #       label: "done_tutorial"
-        #     }
-        #   }
-        # }
-        # @see https://github.com/rails/rails/blob/master/activerecord/lib/active_record/enum.rb
-        def enum_to_lookml
-          defined_enums.map do |name, enum_values|
-            when_lines_lookml = enum_values.map do |label, value|
-              <<-LOOKML
-      when: {
-        sql: ${TABLE}.#{name} = #{value} ;;
-        label: "#{label}"
-      }
-              LOOKML
-            end.join
-
-            <<-LOOKML
-  dimension: #{name} {
-    case: {
-  #{when_lines_lookml}
-    }
-  }
-            LOOKML
-          end.join("\n")
-        end
-
         def to_lookml(table_name_prefix: 'wantedly-1371.rdb.pulse_')
           fields = attribute_types.map do |attribute, type|
             attribute_type_to_set_detail_field(attribute, type)
@@ -117,6 +42,7 @@ module ActiveRecord
               b << Field.new(name: "sql", value: "${TABLE}.#{attribute} ;;")
             end
           when ActiveRecord::Enum::EnumType
+            # @see https://github.com/rails/rails/blob/master/activerecord/lib/active_record/enum.rb
             enum_values = defined_enums[attribute]
 
             Block.new(type: "dimension", name: attribute) do |b|
--- spec/active_record/lookml/core_spec.rb
+++ spec/active_record/lookml/core_spec.rb
@@ -1,82 +1,8 @@
 require 'models/pulse_onboarding_status'
 
 RSpec.describe ActiveRecord::LookML::Core do
-  it "adds .enum_to_lookml method to ActiveRecord::Base" do
-    expect(ActiveRecord::Base.enum_to_lookml).to eq ""
-  end
-
-  describe ".enum_to_lookml" do
-    let(:lookml) do
-      <<-LOOKML
-  dimension: manager_tutorial_state {
-    case: {
-        when: {
-        sql: ${TABLE}.manager_tutorial_state = 0 ;;
-        label: "start_tutorial"
-      }
-      when: {
-        sql: ${TABLE}.manager_tutorial_state = 1 ;;
-        label: "explain_condition_survey"
-      }
-      when: {
-        sql: ${TABLE}.manager_tutorial_state = 2 ;;
-        label: "ask_condition"
-      }
-      when: {
-        sql: ${TABLE}.manager_tutorial_state = 3 ;;
-        label: "explain_high_fives"
-      }
-      when: {
-        sql: ${TABLE}.manager_tutorial_state = 4 ;;
-        label: "try_high_fives"
-      }
-      when: {
-        sql: ${TABLE}.manager_tutorial_state = 5 ;;
-        label: "request_members_to_get_started"
-      }
-      when: {
-        sql: ${TABLE}.manager_tutorial_state = 6 ;;
-        label: "done_announce_to_member"
-      }
-      when: {
-        sql: ${TABLE}.manager_tutorial_state = 7 ;;
-        label: "done_tutorial"
-      }
-
-    }
-  }
-
-  dimension: member_tutorial_state {
-    case: {
-        when: {
-        sql: ${TABLE}.member_tutorial_state = 0 ;;
-        label: "explain_condition_survey"
-      }
-      when: {
-        sql: ${TABLE}.member_tutorial_state = 1 ;;
-        label: "ask_condition"
-      }
-      when: {
-        sql: ${TABLE}.member_tutorial_state = 2 ;;
-        label: "explain_high_fives"
-      }
-      when: {
-        sql: ${TABLE}.member_tutorial_state = 3 ;;
-        label: "try_high_fives"
-      }
-      when: {
-        sql: ${TABLE}.member_tutorial_state = 4 ;;
-        label: "done_tutorial"
-      }
-
-    }
-  }
-      LOOKML
-    end
-
-    it "returns partial lookml defining dimensions for enum attributes" do
-      expect(PulseOnboardingStatus.enum_to_lookml).to eq lookml
-    end
+  it "adds .to_lookml method to ActiveRecord::Base" do
+    expect(ActiveRecord::Base.to_lookml).to eq ""
   end
 
   describe ".to_lookml" do
